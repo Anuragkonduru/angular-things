daa764ac6d6f1db04ddd654a1789f25fba580f40
AnuragKonduru <konduruanurag@gmail.com>
2025-06-21 18:06:09 +0530

    moved comoponents from my old repository

--- src/app/things/modules/misc/components/navigation/constants/navigation.ts
+++ src/app/things/modules/misc/components/navigation/constants/navigation.ts
@@ -1,4 +1,11 @@
 export const NAVIGATION_LINK = [
   { name: 'Misc Home', link: '/misc/misc-landing' },
   { name: 'observables', link: '/misc/observables' },
+  { name: 'promises', link: '/misc/promises' },
+  { name: 'inputoutput', link: '/misc/inputoutput' },
+  { name: 'http', link: '/misc/http' },
+  { name: 'subjects', link: '/misc/subjects' },
+  { name: 'forms', link: '/misc/forms' },
+  { name: 'observables', link: '/misc/observables' },
+  { name: 'lifecycle', link: '/misc/lifecycle' },
 ];
--- src/app/things/modules/misc/misc-routing.module.ts
+++ src/app/things/modules/misc/misc-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsComponent } from './components/forms/forms.component';
+import { HttpComponent } from './components/http/http.component';
+import { InputOutputComponent } from './components/input-output/input-output.component';
+import { LifecycleComponent } from './components/lifecycle/lifecycle.component';
 import { ObservableComponent } from './components/observable/observable.component';
+import { PromisesComponent } from './components/promises/promises.component';
+import { SubjectsComponent } from './components/subjects/subjects.component';
 import { MiscComponent } from './misc.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'misc-landing', pathMatch: 'full' },
   { path: 'misc-landing', component: MiscComponent },
   { path: 'observables', component: ObservableComponent },
+  { path: 'promises', component: PromisesComponent },
+  { path: 'inputoutput', component: InputOutputComponent },
+  { path: 'http', component: HttpComponent },
+  { path: 'subjects', component: SubjectsComponent },
+  { path: 'forms', component: FormsComponent },
+  { path: 'lifecycle', component: LifecycleComponent },
 ];
 
 @NgModule({
